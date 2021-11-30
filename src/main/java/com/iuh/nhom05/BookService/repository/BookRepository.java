459de9a885096f0883de098874d4448112cac6f7
catluynh <catluynh99@gmail.com>
2021-11-30 22:10:23 +0700

    reset project

--- src/main/java/com/iuh/nhom05/BookService/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iuh.nhom05.BookService.repository;
-
-import com.iuh.nhom05.BookService.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
--- src/main/java/com/iuh/nhom05/BookService/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.iuh.nhom05.BookService.service;
-
-import com.iuh.nhom05.BookService.vo.Book_Athour_VO;
-
-public interface BookService {
-    public Book_Athour_VO findById_vo(int id);
-}
