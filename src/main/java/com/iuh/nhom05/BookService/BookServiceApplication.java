610244e98b69113a6816085a0b77a8290f628c2e
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-12-08 13:59:06 +0700

    Feat: Swagger

--- src/main/java/com/iuh/nhom05/BookService/BookServiceApplication.java
+++ src/main/java/com/iuh/nhom05/BookService/BookServiceApplication.java
@@ -4,8 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +23,28 @@ public class BookServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		// Return a prepared Docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/books/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.iuh.nhom05"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Book Service API",
+				"APIs for Book Service",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Nhóm 05", "http://fit.iuh.edu.vn/home.html", "a@b.com"),
+				"API License",
+				"http://fit.iuh.edu.vn/home.html",
+				Collections.emptyList());
 	}
 }
--- src/main/java/com/iuh/nhom05/BookService/entities/Book.java
+++ src/main/java/com/iuh/nhom05/BookService/entities/Book.java
@@ -1,27 +1,36 @@
 package com.iuh.nhom05.BookService.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
-@Entity
+//@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@ApiModel(description = "Details about the book")
 public class Book {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The unique id of the author")
     private int id;
+    @ApiModelProperty(notes = "The book's imageUrl")
     private String imageUrl;
+    @ApiModelProperty(notes = "The book's title")
     private String title;
+    @ApiModelProperty(notes = "The book's authorId")
     private int authorId;
+    @ApiModelProperty(notes = "The book's price")
     private double price;
+    @ApiModelProperty(notes = "The book's publishedYear")
     private int publishedYear;
 }
